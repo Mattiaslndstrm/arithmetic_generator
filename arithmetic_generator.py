8e6f7a010e346d572b458882c6c36b692df47c4e
Mattias Lindström <mattias.lindstrom@gmail.com>
2019-05-12 10:55:52 +0200

    Change arguments to abbrevations

--- arithmetic_generator.py
+++ arithmetic_generator.py
@@ -8,12 +8,12 @@ def get_command_line_input():
     parser = argparse.ArgumentParser(
          description=('Generates csv-files with arithmetic problems for '
                       'easy import into Anki or other SRS-software.'))
-    parser.add_argument('filename', help='filename for the csv-file')
-    parser.add_argument('operation', choices=('a', 's', 'm', 'd', 'sq'),
+    parser.add_argument('file', help='filename for the csv-file')
+    parser.add_argument('op', choices=('a', 's', 'm', 'd', 'sq'),
                         help='arithmetic operation (add/subtract/multiply/'
                              'divide/square)')
-    parser.add_argument('listlength', type=int, help='number of problems')
-    parser.add_argument('magnitude', type=int,
+    parser.add_argument('length', type=int, help='number of problems')
+    parser.add_argument('magn', type=int,
                         help='order of magnitude (number of digits, 3 = 100-999)')
     parser.add_argument('terms', type=int, help='number of terms')
     parser.add_argument('--single', default=False, action='store_true',
